31bcc9269f0f7e8b24a4285f3844120bbce4f4c9
Ajit Rai <ajit.rai@gmail.com>
2020-02-21 18:19:03 -0500

    my first commit.

---
+++ finance-reatil-mservice/src/main/java/com/springframework/retailservice/FinanceReatilMserviceApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.retailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceReatilMserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceReatilMserviceApplication.class, args);
+	}
+
+}
---
+++ finance-reatil-mservice/src/test/java/com/springframework/retailservice/FinanceReatilMserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.retailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceReatilMserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
